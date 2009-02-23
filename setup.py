77ee4eb8a09bb624721846c9e15f0b64d83182af
Eevee <git@veekun.com>
2009-02-22 23:44:40 -0800

    Moved CSV sources into pokedex/.
    This appears to be the only way to make data access in eggs actually
    work, and is why the Platinum sprites are in pokedex/ too.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
+print find_packages()
 setup(
     name = 'Pokedex',
     version = '0.1',
     packages = find_packages(),
-    package_data = { '': ['data', 'images'] },
+    package_data = { '': ['data/*', 'images'] },
 
     entry_points = {
         'console_scripts': [
