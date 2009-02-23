f67cf73581527a1c9176f3d207c8f6187c61cfcb
Eevee <git@veekun.com>
2009-02-22 23:46:25 -0800

    Reverted unnecessary changes to setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
-print find_packages()
 setup(
     name = 'Pokedex',
     version = '0.1',
     packages = find_packages(),
-    package_data = { '': ['data/*', 'images'] },
+    package_data = { '': ['data', 'images'] },
 
     entry_points = {
         'console_scripts': [
