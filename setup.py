238022485a66c6d6920b81d0a6235ab314188a9b
Eevee <git@veekun.com>
2009-08-22 21:50:33 -0700

    Fix whoosh version so Nidoran search works.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version = '0.1',
     packages = find_packages(),
     package_data = { '': 'data' },
-    install_requires=['SQLAlchemy>=0.5.1', 'whoosh>=0.3.0b1'],
+    install_requires=['SQLAlchemy>=0.5.1', 'whoosh==0.3.0b5'],
 
     entry_points = {
         'console_scripts': [
