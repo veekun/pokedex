fce02ad9eb69ce1645d5f1fb4c125f6a2afc0a7e
Eevee <git@veekun.com>
2009-09-13 20:11:05 -0700

    Fixed up setup.py.  Most notably, 'install' now works.
    The package_data line was still wrong.
    
    Also turned off zip_safe, since it's really not; pokedex setup tries to
    write to the install directory!
    
    Finally, bumped the whoosh version; the beta I required has been taken
    off pypi, and the bugs in b6 seem to be gone now.

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 setup(
     name = 'Pokedex',
     version = '0.1',
+    zip_safe = False,
     packages = find_packages(),
-    package_data = { '': ['data'] },
-    install_requires=['SQLAlchemy>=0.5.1', 'whoosh==0.3.0b5'],
+    package_data = { '': ['pokedex/data'] },
+    install_requires=['SQLAlchemy>=0.5.1', 'whoosh>=0.3.0b24'],
 
     entry_points = {
         'console_scripts': [
