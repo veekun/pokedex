106e156d22896f74fbb89a5e82ac5fd6aa023295
Zhorken <zhorken@gmail.com>
2010-05-26 14:46:01 -0400

    Update the form group description column. #210
    I meant to include this in the last commit.  Whoops.
    
    Rotom's description is *really long*, so I needed to bump the length up
    to fit it.  Also changed it to an RstTextColumn.

--- pokedex/db/tables.py
+++ pokedex/db/tables.py
@@ -543,7 +543,7 @@ class PokemonFormGroup(TableBase):
     __tablename__ = 'pokemon_form_groups'
     pokemon_id = Column(Integer, ForeignKey('pokemon.id'), primary_key=True, nullable=False, autoincrement=False)
     is_battle_only = Column(Boolean, nullable=False)
-    description = Column(Unicode(512), nullable=False)
+    description = Column(rst.RstTextColumn(1024), nullable=False)
 
 class PokemonFormSprite(TableBase):
     __tablename__ = 'pokemon_form_sprites'
