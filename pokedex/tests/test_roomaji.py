c25db1d2cfaa63ff63efec3a5ca5d12765e1ab5e
Petr Viktorin <encukou@gmail.com>
2011-01-26 02:14:42 +0100

    Add Czech romanization

--- pokedex/tests/test_roomaji.py
+++ pokedex/tests/test_roomaji.py
@@ -12,6 +12,7 @@ def test_roomaji():
         # Elongated vowel
         (u'イーブイ',           'iibui'),
         (u'ホーホー',           'hoohoo'),
+        (u'ピカチュウ',         u'pikachuu'),
 
         # Combined characters
         (u'ニャース',           'nyaasu'),
@@ -28,3 +29,28 @@ def test_roomaji():
     for kana, roomaji in tests:
         result = pokedex.roomaji.romanize(kana)
         assert_equal(result, roomaji, u"'%s' romanizes correctly" % roomaji)
+
+def test_roomaji_cs():
+    tests = [
+        (u'ヤミカラス',         u'jamikarasu'),
+
+        # Elongated vowel
+        (u'イーブイ',           u'íbui'),
+        (u'ホーホー',           u'hóhó'),
+        (u'ピカチュウ',         u'pikačú'),
+
+        # Combined characters
+        (u'ニャース',           u'ňjásu'),
+        (u'ジャ',              u'dža'),
+        (u'ぎゃくてん',         u'gjakuten'),
+        (u'ウェザーボール',     u'wezábóru'),
+
+        # Special katakana combinations
+        (u'ラティアス',         u'ratiasu'),
+        (u'ウィー',             u'wí'),
+        (u'セレビィ',           u'serebí'),
+    ]
+
+    for kana, roomaji in tests:
+        result = pokedex.roomaji.romanize(kana, 'cs')
+        assert_equal(result, roomaji, u"'%s' romanizes correctly for Czech" % roomaji)
