6c286ffa6f008a2a8a33c177e245b3b19815eee9
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2011-01-31 14:38:12 -0500

    Morning is not a separate time of day for evolution.
    As far as evolution is concerned, it's always day or night--there is no
    in-between period when Eevee will not evolve into Espeon or Umbreon, and
    there are no Pokémon that only evolve during the early part of the day.
    Having 'morning' as a separate value is thus misleading, albeit not
    terribly misleading since it never appeared outside of tables.py.

--- pokedex/db/tables.py
+++ pokedex/db/tables.py
@@ -984,7 +984,7 @@ class PokemonEvolution(TableBase):
         info=dict(description=u"The ID of the location the evolution must be triggered at."))
     held_item_id = Column(Integer, ForeignKey('items.id'), nullable=True,
         info=dict(description=u"The ID of the item the Pokémon must hold."))
-    time_of_day = Column(Enum('morning', 'day', 'night', name='pokemon_evolution_time_of_day'), nullable=True,
+    time_of_day = Column(Enum('day', 'night', name='pokemon_evolution_time_of_day'), nullable=True,
         info=dict(description=u"The required time of day."))
     known_move_id = Column(Integer, ForeignKey('moves.id'), nullable=True,
         info=dict(description=u"The ID of the move the Pokémon must know."))
