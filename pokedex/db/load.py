987116f662ecb34d1bcfc5b9e9af824543909172
Eevee <git@veekun.com>
2012-03-30 23:33:48 -0700

    Fix duplicate type errors when loading into postgresql.

--- pokedex/db/load.py
+++ pokedex/db/load.py
@@ -150,8 +150,19 @@ def load(session, tables=[], directory=None, drop_tables=False, verbose=False, s
 
     # Drop all tables if requested
     if drop_tables:
+        bind = session.get_bind()
         print_start('Dropping tables')
         for n, table in enumerate(reversed(table_objs)):
+            # Drop columns' types if appropriate; needed for enums in
+            # postgresql
+            for column in table.c:
+                try:
+                    drop = column.type.drop
+                except AttributeError:
+                    pass
+                else:
+                    drop(bind=bind, checkfirst=True)
+
             table.drop(checkfirst=True)
             print_status('%s/%s' % (n, len(table_objs)))
         print_done()
