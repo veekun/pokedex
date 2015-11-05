cf5ea6a43426010a491a42db51e6976cd1c047fe
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-11-05 12:45:58 -0800

    setup.py: Don't require importlib
    Was a hack to keep markdown working on 2.6.

--- setup.py
+++ setup.py
@@ -2,17 +2,6 @@ from setuptools import setup, find_packages
 
 import sys
 
-deps = [
-    'SQLAlchemy>=0.9.7',
-    'whoosh>=2.5,<2.7',
-    'markdown',
-    'construct',
-    'six>=1.9.0',
-]
-if sys.version_info < (2, 7):
-    # We don't actually use this, but markdown does
-    deps.append('importlib')
-
 setup(
     name = 'Pokedex',
     version = '0.1',
@@ -21,8 +10,13 @@ setup(
     package_data = {
         'pokedex': ['data/csv/*.csv']
     },
-    install_requires=deps,
-
+    install_requires = [
+        'SQLAlchemy>=0.9.7',
+        'whoosh>=2.5,<2.7',
+        'markdown',
+        'construct',
+        'six>=1.9.0',
+    ],
     entry_points = {
         'console_scripts': [
             'pokedex = pokedex.main:setuptools_entry',
