969d671c48be1d30845cc2093d8380eef44c9e3d
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2016-08-14 00:15:30 -0700

    Make great strides towards an actual game-aware API
    Huzzah!
    
    Also, I decided to namespace identifiers.  For now.  We'll see how it
    goes.

--- pokedex/schema.py
+++ pokedex/schema.py
@@ -3,20 +3,39 @@
 from collections import defaultdict
 from collections import OrderedDict
 from pprint import pprint
+import types
 
 import camel
-
 
 
 class _Attribute:
     name = None
     _creation_order = 0
+
     def __init__(self):
         self._creation_order = _Attribute._creation_order
         _Attribute._creation_order += 1
 
+    def __get__(self, inst, owner):
+        # TODO this is intended for the glom object, not a slice
+        return self.Glommed(self, inst)
+
     def __set_name__(self, cls, name):
         self.name = name
+
+    class Glommed:
+        def __init__(self, prop, obj):
+            self.prop = prop
+            self.obj = obj
+
+        def __repr__(self):
+            return "<{} of {!r}.{}: {!r}>".format(
+                type(self).__qualname__,
+                self.obj,
+                self.prop.name,
+                {game: getattr(slice, self.prop.name) for game, slice in self.obj._slices.items()},
+            )
+
 
     # TODO classtools, key sort by _creation_order
 
@@ -55,20 +74,48 @@ class _ForwardDeclaration:
     pass
 
 
+class Slice:
+    is_slice = True
+
+    def __init__(self):
+        pass
+
+
 class LocusMeta(type):
-    def __init__(cls, name, bases, attrs):
-        for key, attr in attrs.items():
-            if hasattr(attr, '__set_name__'):
-                attr.__set_name__(cls, key)
-
-        super().__init__(name, bases, attrs)
-        # TODO uhh yeah figure this out.  possibly related to attrs
-        cls.index = {}
-
-        # TODO need default values
+    # This is purely a backport of Python 3.6 functionality, and is taken from
+    # PEP 487.  Once the minimum version supported is 3.6, this metaclass can
+    # go away entirely.
+    if not hasattr(object, '__init_subclass__'):
+        def __new__(cls, *args, **kwargs):
+            if len(args) != 3:
+                return super().__new__(cls, *args)
+            name, bases, ns = args
+            init = ns.get('__init_subclass__')
+            if isinstance(init, types.FunctionType):
+                ns['__init_subclass__'] = classmethod(init)
+            else:
+                init = None
+            self = super().__new__(cls, name, bases, ns)
+            for k, v in self.__dict__.items():
+                func = getattr(v, '__set_name__', None)
+                if func is not None:
+                    func(self, k)
+            sup = super(self, self)
+            if hasattr(sup, '__init_subclass__'):
+                sup.__init_subclass__(**kwargs)
+            return self
 
 
 class Locus(metaclass=LocusMeta):
+    _attributes = {}
+
+    def __init_subclass__(cls, **kwargs):
+        # super().__init_subclass__(**kwargs)
+        cls._attributes = cls._attributes.copy()
+        for key, value in cls.__dict__.items():
+            if isinstance(value, _Attribute):
+                cls._attributes[key] = value
+
     def __init__(self, **kwargs):
         cls = type(self)
 
@@ -78,6 +125,25 @@ class Locus(metaclass=LocusMeta):
 
             setattr(self, key, value)
 
+    def __repr__(self):
+        return "<{}: {}>".format(
+            type(self).__qualname__,
+            self.identifier,
+        )
+
+
+class VersionedLocus(Locus):
+    def __init_subclass__(cls, **kwargs):
+        super(VersionedLocus, cls).__init_subclass__(**kwargs)
+
+        if not issubclass(cls, Slice):
+            class Sliced(cls, Slice):
+                base_class = cls
+
+            # TODO this is a circular reference; do i care?
+            cls.Sliced = Sliced
+
+            cls._slices = {}
 
 # TODO seems to me that each of these, regardless of whether they have any
 # additional data attached or not, are restricted to a fixed extra-game-ular
@@ -91,10 +157,9 @@ MoveSet = _ForwardDeclaration()
 Pokedex = _ForwardDeclaration()
 
 
-
-
-class Pokemon(Locus):
-    # TODO version, language.  but those are kind of meta-fields; do they need treating specially?
+class Pokémon(VersionedLocus):
+    # TODO version, language.  but those are kind of meta-fields; do they need
+    # treating specially?
     # TODO in old games, names are unique per game; in later games, they differ
     # per language.  what do i do about that?
     name = _Value(str)
@@ -125,19 +190,34 @@ class Pokemon(Locus):
     # TODO should this be written in hex, maybe?
     game_index = _Value(int)
 
+Pokemon = Pokémon
+
 
 class Repository:
     def __init__(self):
-        # type -> identifier -> list of objects
-        self.objects = defaultdict(lambda: defaultdict(set))
+        # type -> identifier -> object
+        self.objects = defaultdict(lambda: {})
         # type -> property -> value -> list of objects
         self.index = defaultdict(lambda: defaultdict(lambda: defaultdict(set)))
 
     def add(self, obj):
         # TODO this should be declared by the type itself, obviously
         cls = type(obj)
-        self.objects[cls][obj.identifier].add(obj)
-        self.index[cls][cls.name][obj.name].add(obj)
+        # TODO both branches here should check for duplicates
+        if isinstance(obj, Slice):
+            cls = cls.base_class
+            if obj.identifier not in self.objects[cls]:
+                glom = cls()
+                glom.identifier = obj.identifier
+                self.objects[cls][obj.identifier] = glom
+            else:
+                glom = self.objects[cls][obj.identifier]
+            # TODO this...  feels special-cased, but i guess, it is?
+            glom._slices[obj.game] = obj
+        else:
+            self.objects[cls][obj.identifier] = obj
+        # TODO this is more complex now that names are multi-language
+        #self.index[cls][cls.name][obj.name].add(obj)
 
     def fetch(self, cls, identifier):
         # TODO wrap in a...  multi-thing
@@ -161,7 +241,8 @@ def _dump_locus(locus):
 
 @POKEDEX_TYPES.loader('pokemon', version=None)
 def _load_locus(data, version):
-    cls = Pokemon
+    cls = Pokemon.Sliced
+    # TODO wrap with a writer thing?
     obj = cls()
     for key, value in data.items():
         key = key.replace('-', '_')
@@ -176,26 +257,24 @@ def _temp_main():
 
     # just testing for now
     cam = camel.Camel([POKEDEX_TYPES])
-    PATH = 'pokedex/data/gen1/red/en/pokemon.yaml'
+    PATH = 'pokedex/data/ww-red/pokemon.yaml'
     with open(PATH) as f:
         all_pokemon = cam.load(f.read())
         for identifier, pokemon in all_pokemon.items():
             # TODO i don't reeeally like this, but configuring a camel to do it
             # is a little unwieldy
-            pokemon.version = 'red'
-            pokemon.language = 'en'
+            pokemon.game = 'ww-red'
             # TODO this in particular seems extremely clumsy, but identifiers ARE fundamentally keys...
             pokemon.identifier = identifier
 
             repository.add(pokemon)
-    PATH = 'pokedex/data/gen1/red/fr/pokemon.yaml'
+    PATH = 'pokedex/data/ww-blue/pokemon.yaml'
     with open(PATH) as f:
         all_pokemon = cam.load(f.read())
         for identifier, pokemon in all_pokemon.items():
             # TODO i don't reeeally like this, but configuring a camel to do it
             # is a little unwieldy
-            pokemon.version = 'red'
-            pokemon.language = 'fr'
+            pokemon.game = 'ww-blue'
             # TODO this in particular seems extremely clumsy, but identifiers ARE fundamentally keys...
             pokemon.identifier = identifier
 
@@ -206,11 +285,12 @@ def _temp_main():
     # - but what about the vast majority of properties that are the same in every language and only vary by version?
     # - what about later games, where only some properties vary by language?  in the extreme case, xy/oras are single games!
 
-    eevee = repository.fetch(Pokemon, 'eevee')
+    # TODO should this prepend the prefix automatically...  eh
+    eevee = repository.fetch(Pokemon, 'pokemon.eevee')
     pprint(eevee)
-    return
-    eevee = Pokemon['eevee']
+    # TODO i feel like this should work: eevee = repository.Pokemon['eevee']
     print(eevee.name)
+    print(eevee.types)
 
 
     # TODO alright so we need to figure out the "index" part, and how you
