9c25b268ebb77a95c16f86b204898ab042a5a7e3
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2020-03-04 15:07:03 -0800

    Update README and improve docker support (#294)
    * improve python 3, docker support and README
    
    - fix support for python 3.7
    - simplify docker support
    - update readme
    - remove obsolete item-questions file

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
 from setuptools import setup, find_packages
 
-import sys
-
 setup(
-    name = 'Pokedex',
-    version = '0.1',
-    zip_safe = False,
-    packages = find_packages(),
-    package_data = {
+    name='Pokedex',
+    version='0.1',
+    zip_safe=False,
+    packages=find_packages(),
+    package_data={
         'pokedex': ['data/csv/*.csv']
     },
-    install_requires = [
-        'SQLAlchemy>=0.9.7',
+    install_requires=[
+        'SQLAlchemy>=1.0,<2.0',
         'whoosh>=2.5,<2.7',
         'markdown==2.4.1',
-        'construct<=2.5.3',
+        'construct==2.5.3',
         'six>=1.9.0',
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'pokedex = pokedex.main:setuptools_entry',
         ],
     },
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-    ],
+        "Programming Language :: Python :: 3.7",
+    ]
 )
