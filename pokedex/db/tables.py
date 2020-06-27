5971b6ded53fd9d6d4568e4452b205f3c96e6faf
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2020-06-27 11:43:49 -0700

    Move is_legendary & is_mythical columns and re-dump
    Order should always be at the end.

--- pokedex/db/tables.py
+++ pokedex/db/tables.py
@@ -2055,14 +2055,14 @@ class PokemonSpecies(TableBase):
         doc=u"ID of the growth rate for this family")
     forms_switchable = Column(Boolean, nullable=False,
         doc=u"True iff a particular individual of this species can switch between its different forms.")
+    is_legendary = Column(Boolean, nullable=False,
+        doc=u'True iff the Pokémon is a legendary Pokémon.')
+    is_mythical = Column(Boolean, nullable=False,
+        doc=u'True iff the Pokémon is a mythical Pokémon.')
     order = Column(Integer, nullable=False, index=True,
         doc=u'The order in which species should be sorted.  Based on National Dex order, except families are grouped together and sorted by stage.')
     conquest_order = Column(Integer, nullable=True, index=True,
         doc=u'The order in which species should be sorted for Pokémon Conquest-related tables.  Matches gallery order.')
-    is_legendary = Column(Boolean, nullable=False,
-        doc=u'True if the Pokémon is a legendary Pokémon.')
-    is_mythical = Column(Boolean, nullable=False,
-        doc=u'True if the Pokémon is a mythical Pokémon.')
 
 create_translation_table('pokemon_species_names', PokemonSpecies, 'names',
     relation_lazy='joined',
