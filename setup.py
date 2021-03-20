3eb213aab41d7fe5dd76600c869e5684049255de
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2021-03-20 01:16:04 -0700

    Update list of supported python versions

--- setup.py
+++ setup.py
@@ -22,8 +22,10 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ]
 )
