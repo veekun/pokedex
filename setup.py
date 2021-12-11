2d545de5bf8ba484f2d0bcb4a42b25bf2863e523
Parnassius <Parnassius@users.noreply.github.com>
2021-12-11 01:37:10 +0100

    SQLAlchemy 1.4 support

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         'pokedex': ['data/csv/*.csv']
     },
     install_requires=[
-        'SQLAlchemy>=1.0,<1.4',
+        'SQLAlchemy>=1.0,<2.0',
         'whoosh>=2.5,<2.7',
         'markdown>=2.4.1,<=2.6.11',
         'construct==2.5.3',
